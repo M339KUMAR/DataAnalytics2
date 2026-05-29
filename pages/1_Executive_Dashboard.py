840d5cfef1f519a663444c7de1373ffe6542a199
M339KUMAR <147836243+M339KUMAR@users.noreply.github.com>
2026-05-29 10:55:25 +0530

    Update 1_Executive_Dashboard.py

--- pages/1_Executive_Dashboard.py
+++ pages/1_Executive_Dashboard.py
@@ -279,8 +279,7 @@ with col1:
 with col2:
 
     dept_attrition = (
-        filtered_df2.groupby("Department")
-        ["Attrition_Flag"]
+        filtered_df2.groupby("Department")["Attrition_Flag"]
         .mean()
         .reset_index()
     )
