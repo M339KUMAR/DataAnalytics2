edb6df54299fd32b5b555b465ce5244622be4f00
M339KUMAR <147836243+M339KUMAR@users.noreply.github.com>
2026-05-29 05:31:48 +0530

    Update Executive_Dashboard.py

--- pages/Executive_Dashboard.py
+++ pages/Executive_Dashboard.py
@@ -1,1 +1,20 @@
 
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+
+# -----------------------------------------------------
+# PAGE CONFIG
+# -----------------------------------------------------
+st.set_page_config(
+    page_title="Executive Dashboard",
+    page_icon="📊",
+    layout="wide"
+)
+
+st.title("📊 Executive HR Dashboard")
+st.markdown("Monitor employee attrition risk and workforce insights")
