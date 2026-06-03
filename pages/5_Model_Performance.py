75c9b52abfea8ed1d9cee0820d7fe9937fcd8433
M339KUMAR <147836243+M339KUMAR@users.noreply.github.com>
2026-06-04 05:06:57 +0530

    Update 5_Model_Performance.py

--- pages/5_Model_Performance.py
+++ pages/5_Model_Performance.py
@@ -345,7 +345,7 @@ st.dataframe(
         "{:.3f}"
     )
 )
-st.write("Definetly from above table XGBOOST model has better performing Metrics But as a standard i have selected Random Forest Metrics for Display")
+st.write("* Definetly from above table XGBOOST model has better performing Metrics But as a standard i have selected Random Forest Metrics for Display")
 #-------------------------------------------------------------------------------------------------------------------------------------------------
 st.subheader(
     "Random Forest Metrics"
