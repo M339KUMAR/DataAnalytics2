430c902089b92c2854d0e7337a66ed135d26ed9f
M339KUMAR <147836243+M339KUMAR@users.noreply.github.com>
2026-06-07 09:58:42 +0530

    Update 4_Employee_Gender_Analysis.py

--- pages/4_Employee_Gender_Analysis.py
+++ pages/4_Employee_Gender_Analysis.py
@@ -25,7 +25,45 @@ page_bg = """
         </style>
        """
 st.markdown(page_bg, unsafe_allow_html=True)
+#--------------------------------------
+#Metric Cards Customization-->
+# CSS for colorful metric cards
+st.markdown("""
+<style>
 
+/* Metric card styling */
+div[data-testid="stMetric"] {
+    background-color: Teal;
+    padding: 20px;
+    border-radius: 15px;
+    border: 2px solid #FFB6C1;
+    box-shadow: 2px 2px 10px rgba(0,0,0,0.15);
+    text-align: center;
+}
+
+/* Metric label */
+div[data-testid="stMetricLabel"] {
+    color: #6A1B4D;
+    font-size: 25px;
+    font-weight: bold;
+}
+
+/* Metric value */
+div[data-testid="stMetricValue"] {
+    color: #C2185B;
+    font-size: 32px;
+    font-weight: bold;
+}
+
+/* Delta styling */
+div[data-testid="stMetricDelta"] {
+    font-size: 18px;
+    font-weight: bold;
+}
+
+</style>
+""", unsafe_allow_html=True)
+#--------------------------------------
 
 #------------------Load Dataframe----------------
 # --------------------------------------------------
