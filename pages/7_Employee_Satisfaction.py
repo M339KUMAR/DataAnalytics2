3a1298641282f88a5e366e11f0da64718d20a0fe
M339KUMAR <147836243+M339KUMAR@users.noreply.github.com>
2026-06-13 15:46:08 +0530

    Update 7_Employee_Satisfaction.py

--- pages/7_Employee_Satisfaction.py
+++ pages/7_Employee_Satisfaction.py
@@ -1,3 +1,25 @@
 
 
 import strewmlit as st
+
+
+
+from Auth import check_login
+check_login()
+#-----------------------------------------
+st.set_page_config(
+    page_title="Model Performance",
+    page_icon="📈",
+    layout="wide"
+)
+
+st.title("📈 Model Performance Evaluation")
+#--------------------------------------------
+page_bg = """
+       <style>
+       [data-testid="stAppViewContainer"] {
+       background-color: lightgreen;
+       }
+        </style>
+       """
+st.markdown(page_bg, unsafe_allow_html=True)
